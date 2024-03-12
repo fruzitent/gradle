3423fc41eb153174ad3e369b66b79288a8402d27
fruzitent <fruzit@gmail.com>
2024-03-12 18:51:27 +0200

    Upgrade embedded Kotlin from 1.9.22 to 2.0.0-Beta4

--- platforms/documentation/docs/src/snippets/kotlinDsl/androidSingleBuild/kotlin/build.gradle.kts
+++ platforms/documentation/docs/src/snippets/kotlinDsl/androidSingleBuild/kotlin/build.gradle.kts
@@ -2,7 +2,7 @@
 plugins {
     id("com.android.application") version "7.3.0"
 // end::android[]
-    kotlin("android") version "1.9.22"
+    kotlin("android") version "2.0.0-Beta4"
 // tag::android[]
 }
 
